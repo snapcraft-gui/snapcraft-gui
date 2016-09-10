3d8365c45161b10840e19ffc97346d17764d623a
Simon Quigley <tsimonq2@ubuntu.com>
2016-09-10 18:38:04 -0500

    Fix NO_PUBKEY when LATESTQT=yes

--- .travis.sh
+++ .travis.sh
@@ -2,6 +2,7 @@
 
 if [[ $LATESTQT == "yes" ]]; then
 	echo "deb-src http://httpredir.debian.org/debian experimental main" >> /etc/apt/sources.list &&
+	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 7638D0442B90D010 8B48AD6246925553
 	apt update &&
 	apt -t experimental -y install build-essential qt5-default qttools5-dev-tools qttools5-dev
 else
